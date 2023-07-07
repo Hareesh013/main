d76930864216d89e61de3fdd25eb4ed8eb7e31ce
Hareesh013 <uppalahareesh013@gmail.com>
2023-07-07 15:56:45 +0530

    first commit

---
+++ addtask.js
@@ -0,0 +1,12 @@
+function addtask() {
+    if (inputbox.value === '') {
+        alert("you must write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listcontainer.appendchild(li);
+    }
+    
+}
+
